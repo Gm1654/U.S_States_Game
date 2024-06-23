714dafc40b32ef4fb760d3cf3711f99341ec3ba9
Ghulam e mustafa <109214910+Gm1654@users.noreply.github.com>
2024-06-23 01:14:03 -0700

    Update main.py

--- 005 us-states-game-start/us-states-game-start/main.py
+++ 005 us-states-game-start/us-states-game-start/main.py
@@ -13,10 +13,8 @@ guessed_states=[]
 while len(guessed_states)<50:
     answer_states=screen.textinput(title=f"{len(guessed_states)}/50 States correct",prompt="Whats the another states name").title()
     if answer_states=="Exit":
-        missing_states=[]
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states=[state for state in all_states if state not in guessed_states]
+        missing_states.append(state)
         new_data=pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn")      
         break
